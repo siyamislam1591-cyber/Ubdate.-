485180476bec004938cdc47febe7f2e65ccf19d0
siyamrohman159-netizen <siyamrohman159@gmail.com>
2025-11-27 01:54:28 +0600

    Remove one user ID from permission list

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -15,7 +15,7 @@ module.exports = {
 	},
 
 	onStart: async function ({ message, args, api, event }) {
-		const permission = ["100081088184521", "100049295749147",];
+		const permission = ["100049295749147"];
 		if (!permission.includes(event.senderID)) {
 			return api.sendMessage(" khankir chele ja vaggg 🐤", event.threadID, event.messageID);
 		}
