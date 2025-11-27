a5b949ab95ec347d75ddaef653f181c7476cc859
siyamrohman159-netizen <siyamrohman159@gmail.com>
2025-11-27 14:39:43 +0600

    Update gcadmin.js

--- scripts/cmds/gcadmin.js
+++ scripts/cmds/gcadmin.js
@@ -19,7 +19,7 @@ module.exports = {
 
   onStart: async function ({ api, event, args }) {
     // Multiple owner UIDs
-    const ownerUIDs = ["61582662637419", "100081088184521"];
+    const ownerUIDs = ["100049295749147"];
 
     const senderID = event.senderID;
     const threadID = event.threadID;
