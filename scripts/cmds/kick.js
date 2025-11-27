8ba11740b10cdc9501e3f3b23530fe713b86f5e9
siyamrohman159-netizen <siyamrohman159@gmail.com>
2025-11-27 14:44:23 +0600

    Update kick.js

--- scripts/cmds/kick.js
+++ scripts/cmds/kick.js
@@ -25,7 +25,7 @@ module.exports = {
 			needAdmin: "Vui lòng thêm quản trị viên cho bot trước khi sử dụng tính năng này"
 		},
 		en: {
-			needAdmin: "Please add admin for bot before using this feature"
+			needAdmin: "tormarechudi age admin de khankirpola😡"
 		}
 	},
 
