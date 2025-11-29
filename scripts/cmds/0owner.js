7c67bbbdc614ebc100971881d6a031bcdf9585ee
siyamrohman159-netizen <siyamrohman159@gmail.com>
2025-11-29 17:34:26 +0600

    Update 0owner.js

--- scripts/cmds/0owner.js
+++ scripts/cmds/0owner.js
@@ -4,10 +4,10 @@ module.exports = {
   config: {
     name: "owner",
     aliases: ["info", "admininfo", "ownerinfo"],
-    version: "2.2",
-    author: "Jani nh ke manger nati cng marche 🙂",
+    version: "2.4",
+    author: "Shahariya Ahmed Siyam (Siyuu) 🌟",
     longDescription: {
-      en: "Info about bot and owner"
+      en: "Info about bot and its owner"
     },
     category: "Special",
     guide: {
@@ -29,11 +29,10 @@ module.exports = {
   },
 
   sendOwnerInfo: async function ({ event, message, usersData }) {
-    const videoURL = "https://video.xx.fbcdn.net/v/t42.3356-2/501304392_10079169508772619_3565141936249234512_n.mp4?_nc_cat=110&_nc_cb=47395efc-686078dc&ccb=1-7&_nc_sid=4f86bc&_nc_ohc=TeTRZ6zjL3AQ7kNvwHlmFjr&_nc_oc=Adl8A-1ryYV3GVLXH28UFS-Vnba2jW1FtGnz0eLLPtPQPxqEg8Zatidsy6fOYXY0uwA&_nc_zt=28&_nc_ht=video.xx&_nc_gid=NiIdyM7VDQSabBLfsc0ENw&oh=03_Q7cD2QF5s41P2Qo_rLTddJTAqM4SXXjkquHv4sdgrdu_bjDT4A&oe=68398048&dl=1";
+    const videoURL = "https://files.catbox.moe/beh7nq.mp4";
 
     let attachment = null;
     try {
-      // চেক করছি লিঙ্ক ঠিক আছে কি না
       if (videoURL && videoURL.startsWith("http")) {
         attachment = await getStreamFromURL(videoURL);
       }
@@ -47,16 +46,17 @@ module.exports = {
     const mentions = [{ id, tag: name }];
 
     const info = `
-⫷          O᩶w᩶n᩶e᩶r᩶ I᩶n᩶f᩶o᩶          ⫸
-┃ ☁️ 𝗡𝗮𝗺𝗲:          shahariya Ahmed siyam (siyuu)
-┃ ⚙️ 𝗕𝗼𝘁 𝗡𝗮𝗺𝗲:     ♡maiko♡
-┃ 🎂 𝗔𝗴𝗲:            -+
-┃ 🧠 𝗖𝗹𝗮𝘀𝘀:          -+
-┃ ❤️ 𝗥𝗲𝗹𝗮𝘁𝗶𝗼𝗻:      Single 
-┃ ♂️ 𝗚𝗲𝗻𝗱𝗲𝗿:        Male
-┃ 🏠 𝗙𝗿𝗼𝗺:          mymensingh 
-┃ 💬 𝗠𝗲𝘀𝘀𝗲𝗻𝗴𝗲𝗿:     https://m.me/your.cutie.bbz
-♡ 𝐓𝐡𝐚𝐧𝐤𝐬 𝐟𝐨𝐫 𝐮𝐬𝐢𝐧𝐠 𝐦𝐲 𝐛𝐨𝐭 ♡
+🌟✨ 𝗢𝘄𝗻𝗲𝗿 𝗜𝗻𝗳𝗼 ✨🌟
+💠 𝗡𝗮𝗺𝗲:       Shahariya Ahmed Siyam (Siyuu)
+🤖 𝗕𝗼𝘁 𝗡𝗮𝗺𝗲:   ♡Maiko♡
+🎉 𝗔𝗴𝗲:        -+
+🧠 𝗖𝗹𝗮𝘀𝘀:      -+
+💖 𝗥𝗲𝗹𝗮𝘁𝗶𝗼𝗻:   Single
+♂️ 𝗚𝗲𝗻𝗱𝗲𝗿:     Male
+🏡 𝗙𝗿𝗼𝗺:       Mymensingh
+💬 𝗠𝗲𝘀𝘀𝗲𝗻𝗴𝗲𝗿: Ahammed siyam 
+
+🎈 𝗧𝗵𝗮𝗻𝗸𝘀 𝗳𝗼𝗿 𝘂𝘀𝗶𝗻𝗴 𝗺𝘆 𝗯𝗼𝘁 ! Enjoy 🌈
     `.trim();
 
     const msgData = {
